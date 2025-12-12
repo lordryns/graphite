ac38b55aa343ebf6dad1a9bf0d352fa0126ca2e7
David <rynstheoverlord@gmail.com>
2025-12-12 11:32:41 +0100

    added icon support and structure

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 struct slot {
     char *name; 
     char *icon;
+    char *on_click_command;
 };
 
 typedef struct
@@ -34,22 +35,32 @@ static int handler(void* user, const char* section, const char* name,
         pconfig->slot1.name = strdup(value);
     } else if (MATCH("slot-1", "icon")) {
         pconfig->slot1.icon = strdup(value);
+    } else if (MATCH("slot-1", "on_click_command")) {
+        pconfig->slot1.on_click_command = strdup(value);
     } else if (MATCH("slot-2", "name")) {
         pconfig->slot2.name = strdup(value);
+    } else if (MATCH("slot-2", "on_click_command")) {
+        pconfig->slot2.on_click_command = strdup(value);
     } else if (MATCH("slot-2", "icon")) {
         pconfig->slot2.icon = strdup(value);
     } else if (MATCH("slot-3", "name")) {
         pconfig->slot3.name = strdup(value);
     } else if (MATCH("slot-3", "icon")) {
         pconfig->slot3.icon = strdup(value);
-    }else if (MATCH("slot-4", "name")) {
+    } else if (MATCH("slot-3", "on_click_command")) {
+        pconfig->slot3.on_click_command = strdup(value);
+    } else if (MATCH("slot-4", "name")) {
         pconfig->slot4.name = strdup(value);
     } else if (MATCH("slot-4", "icon")) {
         pconfig->slot4.icon = strdup(value);
-    }else if (MATCH("slot-5", "name")) {
+    } else if (MATCH("slot-4", "on_click_command")) {
+        pconfig->slot4.on_click_command = strdup(value);
+    } else if (MATCH("slot-5", "name")) {
         pconfig->slot5.name = strdup(value);
     } else if (MATCH("slot-5", "icon")) {
         pconfig->slot5.icon = strdup(value);
+    } else if (MATCH("slot-5", "on_click_command")) {
+        pconfig->slot5.on_click_command = strdup(value);
     }
     else {
         return 0;
@@ -60,18 +71,18 @@ static int handler(void* user, const char* section, const char* name,
 bool use_config = true;
 static void on_activate (GtkApplication *app) 
 {
-    configuration config; 
+    configuration config = {0}; 
 
     if (ini_parse("graphite.ini", handler, &config) < 0) {
         log_warn("Unable to load graphite.ini, does it exist?");
         use_config = false;
     } else {
         log_info("Loaded graphite.ini successfully");
-        printf("name: %s\n", config.slot1.name);
     }
 
     GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_file(provider, g_file_new_for_path(use_config ? config.style : "style.css"));
+    // config.style contains garbage if not initialised, later fix 
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(config.style));
 
     GdkDisplay *display = gdk_display_get_default(); 
     gtk_style_context_add_provider_for_display(display, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
@@ -97,9 +108,36 @@ static void on_activate (GtkApplication *app)
     gtk_layer_set_margin(GTK_WINDOW (window), GTK_LAYER_SHELL_EDGE_LEFT, 10);
     GtkWidget *main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5); 
 
-    GtkWidget *button = gtk_button_new_with_label("Graphite");
-    
-    gtk_box_append(GTK_BOX (main_box), button);
+
+    // apparently these null guards dont work as well as i thought they did 
+    // .name returns garbage not null
+
+    if (config.slot1.name != NULL) 
+    {
+        GtkWidget *s1button = gtk_button_new_from_icon_name(config.slot1.name); 
+        gtk_box_append(GTK_BOX (main_box), s1button);
+    }
+    if (config.slot2.name != NULL) 
+    {
+        GtkWidget *s2button = gtk_button_new_from_icon_name(config.slot2.name); 
+        gtk_box_append(GTK_BOX (main_box), s2button);
+    }
+    if (config.slot3.name != NULL)
+    {
+        GtkWidget *s3button = gtk_button_new_from_icon_name(config.slot3.name); 
+        gtk_box_append(GTK_BOX (main_box), s3button);
+    }
+    if (config.slot4.name != NULL)
+    {
+        GtkWidget *s4button = gtk_button_new_from_icon_name(config.slot4.name); 
+        gtk_box_append(GTK_BOX (main_box), s4button);
+    }
+    if (config.slot5.name != NULL)
+    {
+        GtkWidget *s5button = gtk_button_new_from_icon_name(config.slot5.name); 
+        gtk_box_append(GTK_BOX (main_box), s5button);
+    }
+
     gtk_window_set_child(GTK_WINDOW (window), main_box);
     gtk_window_present(GTK_WINDOW (window));
 
